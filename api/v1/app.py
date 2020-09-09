dbf49ac294da89a544902350951e75cbbd31f326
andreshugueth <andreshugueth@gmail.com>
2020-09-09 11:11:24 -0500

    adding error handling 404 on app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """API started"""
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,13 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """not found page 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
